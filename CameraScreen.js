1775173ea9541eb80a578f0062395799325dfedf
ananyaka <ananyaka@stanford.edu>
2021-03-28 17:14:48 +0530

    Added barcode screen navigation

--- CameraScreen.js
+++ CameraScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function App() {
+const CameraScreen = ({navigation}) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
 
@@ -13,9 +13,8 @@ export default function App() {
     })();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  const handleBarCodeScanned = ({data}) => {
+    navigation.navigate("Item", { barcodeNumber: data})
   };
 
   if (hasPermission === null) {
@@ -31,7 +30,7 @@ export default function App() {
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      {scanned}
     </View>
   );
 }
@@ -79,4 +78,6 @@ const styles = {
     justifyContent: 'center'
   }
 };
+
+export default CameraScreen;
 
